2a7e7c035843ceaadf6706a64bd7a48580610499
Filip <filip.lusnia@onet.pl>
2024-02-03 17:53:17 +0100

    task_2 wip

--- task_1/script.js
+++ task_1/script.js
@@ -1,11 +1,10 @@
+//desktop
 const makeupHeaderBtn = document.querySelector('.header_nav_btn.-makeup')
 const makeupHeaderMenu = document.querySelector('.header_nav_menu.-makeup')
 const faceHeaderBtn = document.querySelector('.header_nav_btn.-face')
 const faceHeaderMenu = document.querySelector('.header_nav_menu.-face')
-const burger = document.querySelector('.header_burger')
-const mobileMenu = document.querySelector('.header_nav_mobile')
 
-function closeAllMenus() {
+function closeAllDesktopMenus() {
 	const headerButtons = document.querySelectorAll('.header_nav_btn')
 	const headerMenus = document.querySelectorAll('.header_nav_menu')
 
@@ -30,7 +29,7 @@ makeupHeaderBtn.addEventListener('click', function() {
 		arrowIcon.classList.remove('-open')
 	)
 	: (
-		closeAllMenus(),
+		closeAllDesktopMenus(),
 		makeupHeaderMenu.classList.add('-open'),
 		arrowIcon.classList.add('-open')
 	)
@@ -44,11 +43,19 @@ faceHeaderBtn.addEventListener('click', function() {
 		arrowIcon.classList.remove('-open')
 	)
 	: (
-		closeAllMenus(),
+		closeAllDesktopMenus(),
 		faceHeaderMenu.classList.add('-open'),
 		arrowIcon.classList.add('-open')
 	)
 })
+
+//mobile
+const burger = document.querySelector('.header_burger')
+const mobileMenu = document.querySelector('.header_nav_mobile')
+const makeupMobileBtn = document.querySelector('.mobile_nav_btn.-makeup')
+const makeupMobileMenu = document.querySelector('.mobile_nav_menu.-makeup')
+const faceMobileBtn = document.querySelector('.mobile_nav_btn.-face')
+const faceMobileMenu = document.querySelector('.mobile_nav_menu.-face')
 
 burger.addEventListener('click', function() {
 	const burgerIcon = burger.querySelector('img');
@@ -60,5 +67,31 @@ burger.addEventListener('click', function() {
 	: (
 		mobileMenu.classList.add('-open'),
 		burgerIcon.src = './assets/close_icon.svg'
+	)
+})
+
+makeupMobileBtn.addEventListener('click', function() {
+	const arrowIcon = makeupMobileBtn.querySelector('img');
+	const isMenuActive = makeupMobileMenu.classList.contains('-open')
+	isMenuActive ? (
+		makeupMobileMenu.classList.remove('-open'),
+		arrowIcon.classList.remove('-open')
+	)
+	: (
+		makeupMobileMenu.classList.add('-open'),
+		arrowIcon.classList.add('-open')
+	)
+})
+
+faceMobileBtn.addEventListener('click', function() {
+	const arrowIcon = faceMobileBtn.querySelector('img');
+	const isMenuActive = faceMobileMenu.classList.contains('-open')
+	isMenuActive ? (
+		faceMobileMenu.classList.remove('-open'),
+		arrowIcon.classList.remove('-open')
+	)
+	: (
+		faceMobileMenu.classList.add('-open'),
+		arrowIcon.classList.add('-open')
 	)
 })
