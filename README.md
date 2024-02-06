cb5e1a820c132839bd07e01e79bfe1ba252e48f6
Filip <filip.lusnia@onet.pl>
2024-02-06 16:55:14 +0100

    task_1 redone

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Simple header menu.\
 Launch the index.html file to see it live.
 
 ### Task 1 Custom
-Enhanced solution from task_1. It has slightly changed HTML to make styling easier and to include responsive mobile menu.\
+Enhanced solution from task_1. It has a changed HTML to make styling easier, add missig elements and to include responsive mobile menu.\
 Launch the index.html file to see it live.
 
 ### Task 2
@@ -32,4 +32,4 @@ CUSTOM_COMMANDS = {
 To submit a command, type it into the input field and press Enter key.\
 Terminal has an option to search through previously called commands with up and down arrow keys. 
 
-To see this task live, it is required to launch an index.html through some sort of live server, for example: [Live Server](https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer)
+To see this task live, it is required to launch the index.html through some sort of live server, for example: [Live Server](https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer)
--- task_1/script.js
+++ task_1/script.js
@@ -1,97 +1,35 @@
-//desktop
-const makeupHeaderBtn = document.querySelector('.header_nav_btn.-makeup')
-const makeupHeaderMenu = document.querySelector('.header_nav_menu.-makeup')
-const faceHeaderBtn = document.querySelector('.header_nav_btn.-face')
-const faceHeaderMenu = document.querySelector('.header_nav_menu.-face')
+const makeupNav = document.querySelector('.nav_section.-makeup')
+const makeupNavMenu = document.querySelector('.nav_section_menu.-makeup')
+const faceNav = document.querySelector('.nav_section.-face')
+const faceNavMenu = document.querySelector('.nav_section_menu.-face')
 
 function closeAllDesktopMenus() {
-	const headerButtons = document.querySelectorAll('.header_nav_btn')
-	const headerMenus = document.querySelectorAll('.header_nav_menu')
-
-	headerButtons.forEach(btn => {
-		const arrowIcon = btn.querySelector('img');
-		if (arrowIcon && arrowIcon.classList.contains('-open')) {
-			arrowIcon.classList.remove('-open')
-		}
-	})
-	headerMenus.forEach(menu => {
+	const allNavs = document.querySelectorAll('.nav_section_menu')
+	allNavs.forEach(menu => {
 		if (menu.classList.contains('-open')) {
 			menu.classList.remove('-open')
 		}
 	})
 }
 
-makeupHeaderBtn.addEventListener('click', function() {
-	const arrowIcon = makeupHeaderBtn.querySelector('img');
-	const isMenuActive = makeupHeaderMenu.classList.contains('-open')
-	isMenuActive ? (
-		makeupHeaderMenu.classList.remove('-open'),
-		arrowIcon.classList.remove('-open')
-	)
-	: (
-		closeAllDesktopMenus(),
-		makeupHeaderMenu.classList.add('-open'),
-		arrowIcon.classList.add('-open')
-	)
+makeupNav.addEventListener('mouseenter', function() {
+	closeAllDesktopMenus()
+	const isMenuActive = makeupNavMenu.classList.contains('-open');
+	!isMenuActive && makeupNavMenu.classList.add('-open')
 })
 
-faceHeaderBtn.addEventListener('click', function() {
-	const arrowIcon = faceHeaderBtn.querySelector('img');
-	const isMenuActive = faceHeaderMenu.classList.contains('-open')
-	isMenuActive ? (
-		faceHeaderMenu.classList.remove('-open'),
-		arrowIcon.classList.remove('-open')
-	)
-	: (
-		closeAllDesktopMenus(),
-		faceHeaderMenu.classList.add('-open'),
-		arrowIcon.classList.add('-open')
-	)
+makeupNavMenu.addEventListener('mouseleave', function() {
+	const isMenuActive = makeupNavMenu.classList.contains('-open');
+	isMenuActive && makeupNavMenu.classList.remove('-open')
 })
 
-//mobile
-const burger = document.querySelector('.header_burger')
-const mobileMenu = document.querySelector('.header_nav_mobile')
-const makeupMobileBtn = document.querySelector('.mobile_nav_btn.-makeup')
-const makeupMobileMenu = document.querySelector('.mobile_nav_menu.-makeup')
-const faceMobileBtn = document.querySelector('.mobile_nav_btn.-face')
-const faceMobileMenu = document.querySelector('.mobile_nav_menu.-face')
-
-burger.addEventListener('click', function() {
-	const burgerIcon = burger.querySelector('img');
-	const isMenuActive = mobileMenu.classList.contains('-open')
-	isMenuActive ? (
-		mobileMenu.classList.remove('-open'),
-		burgerIcon.src = './assets/burger_icon.svg'
-	)
-	: (
-		mobileMenu.classList.add('-open'),
-		burgerIcon.src = './assets/close_icon.svg'
-	)
+faceNav.addEventListener('mouseenter', function() {
+	closeAllDesktopMenus()
+	const isMenuActive = faceNavMenu.classList.contains('-open');
+	!isMenuActive && faceNavMenu.classList.add('-open')
 })
 
-makeupMobileBtn.addEventListener('click', function() {
-	const arrowIcon = makeupMobileBtn.querySelector('img');
-	const isMenuActive = makeupMobileMenu.classList.contains('-open')
-	isMenuActive ? (
-		makeupMobileMenu.classList.remove('-open'),
-		arrowIcon.classList.remove('-open')
-	)
-	: (
-		makeupMobileMenu.classList.add('-open'),
-		arrowIcon.classList.add('-open')
-	)
-})
-
-faceMobileBtn.addEventListener('click', function() {
-	const arrowIcon = faceMobileBtn.querySelector('img');
-	const isMenuActive = faceMobileMenu.classList.contains('-open')
-	isMenuActive ? (
-		faceMobileMenu.classList.remove('-open'),
-		arrowIcon.classList.remove('-open')
-	)
-	: (
-		faceMobileMenu.classList.add('-open'),
-		arrowIcon.classList.add('-open')
-	)
+faceNavMenu.addEventListener('mouseleave', function() {
+	const isMenuActive = faceNavMenu.classList.contains('-open');
+	isMenuActive && faceNavMenu.classList.remove('-open')
 })
