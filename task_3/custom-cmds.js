7e31bb71106ae5c7587b05667ddcd3620919b907
Filip <filip.lusnia@onet.pl>
2024-02-06 14:05:24 +0100

    task 3 done

--- task_3/custom-cmds.js
+++ task_3/custom-cmds.js
@@ -6,8 +6,25 @@ export const CUSTOM_COMMANDS = {
 		desc: 'Prints a welcoming',
 		callback: callbackHello
 	},
+	movies: {
+		name: 'movies',
+		desc: 'Prints a list of movies',
+		callback: callbackMovies
+	},
 }
 
 function callbackHello() {
 	outputNewLine('Hello :)')
+}
+
+function callbackMovies() {
+	fetch('https://my-json-server.typicode.com/horizon-code-academy/fake-movies-api/movies')
+	.then(resp => resp.json())
+	.then(data => {
+		outputNewLine('Movies list:')
+		data.forEach(movie => {
+			outputNewLine(`- ${movie.Title} (${movie.Year})`)
+		});
+	})
+	.catch(err => outputNewLine(err, 'error'))
 }
